ae5e511d3a0cd88ce1e3140fbd2359fe74ae23a7
Dumb Thing <p@w>
2014-09-08 17:59:48 +0200

    loadavg

--- dwmstatus.go
+++ dwmstatus.go
@@ -6,11 +6,37 @@ import "C"
 
 import (
 	"fmt"
-	"os"
+	"io/ioutil"
+	"log"
+	"strings"
 	"time"
 )
 
 var dpy = C.XOpenDisplay(nil)
+
+func getBatteryPercentage(path string) (perc int, err error) {
+	energy_now, err := ioutil.ReadFile(fmt.Sprintf("%s/energy_now", path))
+	if err != nil {
+		return -1, err
+	}
+	energy_full, err := ioutil.ReadFile(fmt.Sprintf("%s/energy_full", path))
+	if err != nil {
+		return -1, err
+	}
+	var enow, efull int
+	fmt.Sscanf(string(energy_now), "%d", &enow)
+	fmt.Sscanf(string(energy_full), "%d", &efull)
+	return enow * 100 / efull, nil
+}
+
+func getLoadAverage(file string) (lavg string, err error) {
+	loadavg, err := ioutil.ReadFile(file)
+	if err != nil {
+		log.Fatal(err)
+		return "Couldn't read loadavg", err
+	}
+	return strings.Join(strings.Fields(string(loadavg))[:3], " "), nil
+}
 
 func setStatus(s *C.char) {
 	C.XStoreName(dpy, C.XDefaultRootWindow(dpy), s)
@@ -22,18 +48,22 @@ func formatStatus(format string, args ...interface{}) *C.char {
 	return C.CString(status)
 }
 
-func die(err string) {
-	fmt.Fprintf(os.Stderr, "%s\n", err)
-	os.Exit(1)
-}
-
 func main() {
 	if dpy == nil {
-		die("Can't open display")
+		log.Fatal("Can't open display")
 	}
 	for {
-		s := formatStatus("%s", time.Now().Format("15:04"))
+		t := time.Now().Format("15:04")
+		b, err := getBatteryPercentage("/sys/class/power_supply/BAT0")
+		if err != nil {
+			log.Fatal(err)
+		}
+		l, err := getLoadAverage("/proc/loadavg")
+		if err != nil {
+			log.Fatal(err)
+		}
+		s := formatStatus("%s :: %s :: %d%%", l, t, b)
 		setStatus(s)
-		time.Sleep(1 * time.Second)
+		time.Sleep(time.Second)
 	}
 }
