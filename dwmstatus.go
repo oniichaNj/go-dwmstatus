69ee4fcdc2dcc88487869d35057409f68cc3a72c
Daniel Langesten <daniel.langest@gmail.com>
2014-10-15 09:02:55 +0200

    fixed bug where n were shadowed

--- dwmstatus.go
+++ dwmstatus.go
@@ -64,7 +64,7 @@ func nowPlaying(addr string) (np string, err error) {
 	arr := strings.Split(string(r), "\n")
 	if arr[8] != "state: play" { //arr[8] is the state according to the mpd documentation
 		status := strings.SplitN(arr[8], ": ", 2)
-		np := fmt.Sprintf("mpd - [%s]", status[1]) //status[1] should now be stopped or paused
+		np = fmt.Sprintf("mpd - [%s]", status[1]) //status[1] should now be stopped or paused
 		return
 	}
 
